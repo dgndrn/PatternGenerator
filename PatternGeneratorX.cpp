e1e68cf28fd1d010b783b77fe36238dff40442e3
dgndrn <celaldoganduran@gmail.com>
2023-07-19 21:10:50 +0300

    Create PatternGeneratorX.cpp

---
+++ PatternGeneratorX.cpp
@@ -0,0 +1,72 @@
+﻿
+#include <iostream>
+using namespace std;
+string pattern;
+
+
+void checker(int size,int counter) {
+
+	if (counter == size) {
+		cout << pattern;
+		exit(0);
+	}
+
+
+}
+
+
+
+
+int main(int argc, char** argv)
+{
+	 int size = atoi(argv[1]);
+
+
+
+
+	char bigA = 'A';
+	char smallA = 'a';
+	char strnum[5];
+	int num = 0; 
+
+	
+	
+	
+
+	while (bigA <= 'Z') {
+
+		while (smallA <= 'z') {
+
+			while (num <= 9) {
+			
+				checker(size, pattern.length());
+
+				_itoa(num, strnum, 10);
+				pattern += bigA;
+				checker(size, pattern.length());
+				pattern += smallA;
+				checker(size, pattern.length());;
+				pattern += strnum;
+				checker(size, pattern.length());
+				num++;
+
+				if (sizeof(pattern) == size) { 
+					return 0;
+				}
+			
+			}
+
+			smallA++;
+			num = 0;
+
+
+		}
+		bigA++;
+		smallA = 'a';
+		num = 0;
+
+	}
+
+
+
+}
