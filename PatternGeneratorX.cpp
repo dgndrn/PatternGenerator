6462077616d9548e29c9c769d627b3c4b7603d1d
dgndrn <celaldoganduran@gmail.com>
2023-07-20 10:05:03 +0300

    Update PatternGeneratorX.cpp

--- PatternGeneratorX.cpp
+++ PatternGeneratorX.cpp
@@ -3,76 +3,38 @@
 using namespace std;
 string pattern;
 
+// type 0 normal degil C++ icin normal olan hepsi arka arkaya onu da yaz. Arguman alırken ucuncu arguman yoksa yazdirmiyor ona bak
 
-void checker(int size,int counter) {
-
-	if (counter == size) {
-		
-		int divide;
-		int x = 0;
-		string piece;
-		int inc;
-		if (pattern.length() > 200) {
-			divide = 85;
-		}
-		else {
-			divide = (pattern.length() / (pattern.length() * 0.1)); // bu aralik icin yani 0 dan 10'a kadar daha sonra 10 dan 20 ye kadar 10 20 30 diye artiyor
-		}
-
-		inc = divide;											// bu uzunluk icin 0 dan 10'a kadar char yazdirilacak 10 tane alt alta olacak gibi
-		cout << "buf = \"\" " << ::endl;
-		for (int j = 0; j < pattern.length(); j=j+inc) {
-		
-			
-			if (inc == 0) {
-				exit(0);                       // burada bitirmemin nedeni program son cout'u yapabilmesi icin donguden sonra bitirmem gerekiyordu
-			}
-			
-			
-			piece = "";
-			for (x; x < divide; x++) {
-				piece += pattern[x];
-			}
-			if ((divide + inc) > pattern.length()) {
-				inc = pattern.length() - divide;
-			}
-
-			divide = divide + inc;
-			
-			cout << "buf+= \"" << piece << "\"" << ::endl;
+void checker(int size, int counter,int type);
+void Generate(int size,int type);
 
 
+// type 0 -> Normal
+// type 1-> Python
 
-		}
+int main(int argc, char** argv)
+{
+	 int size = atoi(argv[1]);
+	 int type;
 
-
-
-
-
-
-
-
-
-
-
-
-
-		/*
-
-
-		cout << "buf = \"\" " <<::endl;
-		for (int i = 0; i <= pattern.length(); i=i+divide) {
-
-			cout << "buf+= \"" << pattern <<"\"" << ::endl;
-		}
-		*/
-		
-
-
-
-
-		exit(0); 
+	string format = argv[2];
+	
+	int decision{ format.compare("-f") };
+	if (!decision) {
+		type = 1;
 	}
+
+	
+
+	else{
+		type = 0;
+	}
+
+
+
+//	int size = 500;
+	
+	Generate(size,type);
 
 
 }
@@ -80,41 +42,40 @@ void checker(int size,int counter) {
 
 
 
-int main(int argc, char** argv)
-{
-	 int size = atoi(argv[1]);
 
-//	int size = 3000;
+
+void Generate(int size,int type) {
+
 
 
 	char bigA = 'A';
 	char smallA = 'a';
 	char strnum[5];
-	int num = 0; 
+	int num = 0;
 
-	
-	
-	
+
+
+
 
 	while (bigA <= 'Z') {
 
 		while (smallA <= 'z') {
 
 			while (num <= 9) {
-			
-				checker(size, pattern.length());
+
+				checker(size, pattern.length(),type);
 
 				_itoa(num, strnum, 10);
 				pattern += bigA;
-				checker(size, pattern.length());
+				checker(size, pattern.length(),type);
 				pattern += smallA;
-				checker(size, pattern.length());;
+				checker(size, pattern.length(),type);
 				pattern += strnum;
-				checker(size, pattern.length());
+				checker(size, pattern.length(),type);
 				num++;
 
 
-			
+
 			}
 
 			smallA++;
@@ -128,6 +89,98 @@ int main(int argc, char** argv)
 
 	}
 
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void checker(int size, int counter,int type) {
+
+	if (counter == size) {
+
+		int divide;
+		int x = 0;
+		string piece;
+		int inc;
+		if (pattern.length() > 200) {
+			divide = 85;
+		}
+		else {
+			divide = (pattern.length() / (pattern.length() * 0.1)); // bu aralik icin yani 0 dan 10'a kadar daha sonra 10 dan 20 ye kadar 10 20 30 diye artiyor
+		}
+
+		inc = divide;											// bu uzunluk icin 0 dan 10'a kadar char yazdirilacak 10 tane alt alta olacak gibi
+		
+		if (type == 1) {
+			cout << "buf = \"\" " << ::endl;
+		}
+		
+		for (int j = 0; j < pattern.length(); j = j + inc) {
+
+
+			if (inc == 0) {
+				exit(0);                       // burada bitirmemin nedeni program son cout'u yapabilmesi icin donguden sonra bitirmem gerekiyordu
+			}
+
+
+			piece = "";
+			for (x; x < divide; x++) {
+				piece += pattern[x];
+			}
+			if ((divide + inc) > pattern.length()) {
+				inc = pattern.length() - divide;
+			}
+
+			divide = divide + inc;
+
+			if (type == 1) {
+				cout << "buf+= \"" << piece << "\"" << ::endl; //python yazimi
+			}
+			else if (type == 0) {
+
+				cout << "\"" << piece << "\"" << ::endl;
+
+			}
+
+
+		}
+
+		/*
+
+
+		cout << "buf = \"\" " <<::endl;
+		for (int i = 0; i <= pattern.length(); i=i+divide) {
+
+			cout << "buf+= \"" << pattern <<"\"" << ::endl;
+		}
+		*/
+
+		exit(0);
+	}
 
 
 }
